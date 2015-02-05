fc98b18a887b9683879e4e0771845d3b017da25b
Thomas Mentzel <thomas.mentzel@schoenhofer.de>
2015-02-05 17:55:48 +0100

    code cleanup.

--- NZazu/NZazuViewExtensions_Should.cs
+++ NZazu/NZazuViewExtensions_Should.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Runtime.InteropServices;
 using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
