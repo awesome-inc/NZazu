2fe2b3e1a1d92d02942284a90a7add02ac0d9b19
mkoertgen <marcel.koertgen@schoenhofer.de>
2015-02-08 13:47:08 +0100

    Replace textboxes by AvalonEdit. added sample code for code completion (FormDefinition) TODO: - add syntax highlightin (Definiion, Data) - get completion right

--- NZazuFiddle/FormDefinitionView.xaml.cs
+++ NZazuFiddle/FormDefinitionView.xaml.cs
@@ -1,10 +1,51 @@
-﻿namespace NZazuFiddle
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+
+namespace NZazuFiddle
 {
     public partial class FormDefinitionView
     {
+        private CompletionWindow _completionWindow;
+
         public FormDefinitionView()
         {
             InitializeComponent();
+
+            TextEditor.TextArea.TextEntering += OnTextEntering;
+            TextEditor.TextArea.TextEntered += OnTextEntered;
+        }
+
+        private void OnTextEntered(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text != ".") return;
+
+            // Open code completion after the user has pressed dot:
+            _completionWindow = new CompletionWindow(TextEditor.TextArea);
+            var data = _completionWindow.CompletionList.CompletionData;
+            data.Add(new MyCompletionData("Item1"));
+            data.Add(new MyCompletionData("Item2"));
+            data.Add(new MyCompletionData("Item3"));
+            _completionWindow.Show();
+            _completionWindow.Closed += delegate
+            {
+                _completionWindow = null;
+            };
+        }
+
+        private void OnTextEntering(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Length > 0 && _completionWindow != null)
+            {
+                if (!char.IsLetterOrDigit(e.Text[0]))
+                {
+                    // Whenever a non-letter is typed while the completion window is open,
+                    // insert the currently selected element.
+                    _completionWindow.CompletionList.RequestInsertion(e);
+                }
+            }
+            // Do not set e.Handled=true.
+            // We still want to insert the character that was typed.        
         }
     }
 }
---
+++ NZazuFiddle/MyCompletionData.cs
@@ -0,0 +1,43 @@
+using System;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+
+namespace NZazuFiddle
+{
+    /// Implements AvalonEdit ICompletionData interface to provide the entries in the
+    /// completion drop down.
+    public class MyCompletionData : ICompletionData
+    {
+        public MyCompletionData(string text)
+        {
+            Text = text;
+        }
+
+        public System.Windows.Media.ImageSource Image
+        {
+            get { return null; }
+        }
+
+        public string Text { get; private set; }
+
+        // Use this property if you want to show a fancy UIElement in the list.
+        public object Content
+        {
+            get { return Text; }
+        }
+
+        public object Description
+        {
+            get { return "Description for " + Text; }
+        }
+
+        public double Priority { get; private set; }
+
+        public void Complete(TextArea textArea, ISegment completionSegment,
+            EventArgs insertionRequestEventArgs)
+        {
+            textArea.Document.Replace(completionSegment, Text);
+        }
+    }
+}
