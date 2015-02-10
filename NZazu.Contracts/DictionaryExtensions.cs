b093d74a7659378baa4c00975d6ab808159b6552
Thomas Mentzel <thomas.mentzel@softwarekueche.de>
2015-02-10 18:49:39 +0100

    dictionary extensions for filtering on items, formdata should not contain empty (NULL) values.

---
+++ NZazu.Contracts/DictionaryExtensions.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NZazu.Contracts
+{
+    public static class DictionaryExtensions
+    {
+        public static Dictionary<TKey, TValue> Remove<TKey, TValue>(
+            this Dictionary<TKey, TValue> source,
+            Func<KeyValuePair<TKey, TValue>, bool> predicate)
+            where TValue : class
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (predicate == null) throw new ArgumentNullException("predicate");
+
+            var validItems = source.Where(predicate);
+            return validItems.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+        }
+    }
+}
