622397f2e4bbb9027096f3f8807f797b7c810fae
mkoertgen <marcel.koertgen@schoenhofer.de>
2015-02-10 22:18:35 +0100

    added options field

---
+++ NZazu/Fields/NZazuOptionsField.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace NZazu.Fields
+{
+    public class NZazuOptionsField : NZazuField<string>
+    {
+        public NZazuOptionsField(string key) : base(key)
+        {
+        }
+
+        public override DependencyProperty ContentProperty
+        {
+            get { return Selector.SelectedItemProperty; }
+        }
+
+        public override string Type { get { return "option"; } }
+
+        public string[] Options { get; protected internal set; }
+
+        protected override Control GetValue()
+        {
+            var control = new ComboBox { ToolTip = Description};
+            if (Options != null)
+            {
+                foreach (var option in Options)
+                    control.Items.Add(option);
+                control.SelectedItem = Options.FirstOrDefault();
+            }
+            return control;
+        }
+
+        protected override void SetStringValue(string value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override string GetStringValue()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ NZazu/Fields/NZazuOptionsField_Should.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace NZazu.Fields
+{
+    [TestFixture]
+    [RequiresSTA]
+    // ReSharper disable InconsistentNaming
+    class NZazuOptionsField_Should
+    {
+        [Test]
+        public void Be_Creatable()
+        {
+            var sut = new NZazuOptionsField("test");
+
+            sut.Should().NotBeNull();
+            sut.Should().BeAssignableTo<INZazuWpfField>();
+            sut.Type.Should().Be("option");
+        }
+
+        [Test]
+        public void Create_ComboBox()
+        {
+            var sut = new NZazuOptionsField("test") { Description = "description"};
+
+            sut.ContentProperty.Should().Be(Selector.SelectedItemProperty);
+            var control = (ComboBox)sut.ValueControl;
+            control.Should().NotBeNull();
+
+            control.ToolTip.Should().Be(sut.Description);
+        }
+
+        [Test]
+        public void Bind_Value_to_SelectedItem()
+        {
+            var sut = new NZazuOptionsField("test")
+            {
+                Description = "description",
+                Options = new[] { "1", "2", "3", "4", "5"}
+            };
+
+            var control = (ComboBox)sut.ValueControl;
+            control.Items.Should().BeEquivalentTo(sut.Options);
+
+            sut.Value.Should().BeNull();
+            control.SelectedItem.Should().BeNull();
+
+            var expected = sut.Options.First();
+            sut.Value = expected;
+            control.SelectedItem.Should().Be(expected);
+
+            expected = sut.Options.Last();
+            control.SelectedItem = expected;
+            sut.Value.Should().Be(expected);
+        }
+    }
+}
