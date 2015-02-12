2cbac9bac2a26120098fddc733cb6f9f16c85c8e
mkoertgen <marcel.koertgen@schoenhofer.de>
2015-02-12 14:05:22 +0100

    removed unused using

--- NZazu/FieldBehavior/BehaviorExtender.cs
+++ NZazu/FieldBehavior/BehaviorExtender.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using FluentAssertions;
-using FluentAssertions.Common;
 using NUnit.Framework;
 
 namespace NZazu.FieldBehavior
