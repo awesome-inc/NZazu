1e50452dd2567c03334f7d07d5c4fe00d90e9b41
Thomas Mentzel <thomas.richard.mentzel@gmail.com>
2016-12-05 12:08:50 +0100

    fixes. view send to behavior using property injection.

--- NZazu/FieldBehavior/NZazuFieldBehavior.cs
+++ NZazu/FieldBehavior/NZazuFieldBehavior.cs
@@ -5,7 +5,7 @@ namespace NZazu.FieldBehavior
     [ExcludeFromConventions("internal and pure abstract")]
     internal abstract class NZazuFieldBehavior : INZazuWpfFieldBehavior
     {
-        public abstract void AttachTo(INZazuWpfField field);
+        public abstract void AttachTo(INZazuWpfField field, INZazuWpfView view);
         public abstract void Detach();
     }
 }
