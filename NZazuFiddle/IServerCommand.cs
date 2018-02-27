f244c863d4411afa769b2c165fbd42e18a240038
Jan Bornemeier <jan.bornemeier@schoenhofer.de>
2018-02-27 08:42:14 +0100

    (tkse) ADD server command MVVM

---
+++ NZazuFiddle/IServerCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZazuFiddle
+{
+    public interface IServerCommand
+    {
+        string Endpoint { get; set; }
+
+        Task<string> GetDataFromEndpoint(string endpoint);
+
+    }
+}
---
+++ NZazuFiddle/ServerCommandViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZazuFiddle
+{
+    class ServerCommandViewModel : IServerCommand
+    {
+        private string _endpointUri = "http://sw-mews-01-app:9200/tacon";
+
+        public string Endpoint
+        {
+            get => _endpointUri;
+            set
+            {
+                if (Equals(value, _endpointUri)) return;
+                _endpointUri = value;
+            }
+
+        }
+
+        public async Task<string> GetDataFromEndpoint(string endpoint)
+        {
+            string result = null;
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(endpoint);
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsStringAsync();
+            }
+            return result;
+        }
+    }
+}
