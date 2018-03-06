8ab8e7b5b8881d13fab4cb8bacfb663e00a2d0a5
Jan Bornemeier <jan.bornemeier@schoenhofer.de>
2018-03-06 14:09:13 +0100

    Add basal loggin utillity function to generate tracing outputs

---
+++ NZazuFiddle/Utils/LoggingUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZazuFiddle.Utils
+{
+    internal static class LoggingUtil
+    {
+
+        public static string CreateLogMessage(Object instance, string msg, string detailedMsg = null, string stackTrace = null)
+        {
+            var message = $"{instance.GetType().FullName} :: {GetCurrentMethod()} => {msg}";
+            message = detailedMsg != null ? message + Environment.NewLine + detailedMsg : message;
+            message = stackTrace != null ? message + Environment.NewLine + stackTrace : message;
+
+            return message;
+        }
+
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static string GetCurrentMethod()
+        {
+            StackTrace st = new StackTrace();
+            StackFrame sf = st.GetFrame(2);
+
+            return sf.GetMethod().Name;
+        }
+    }
+}
