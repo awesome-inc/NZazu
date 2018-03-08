c6651a100117f27b2410e14e6955d7c423a5bafc
Jan Bornemeier <jan.bornemeier@schoenhofer.de>
2018-03-08 18:29:12 +0100

    cleanup

--- NZazuFiddle/TemplateManagement/Data/Session.cs
+++ NZazuFiddle/TemplateManagement/Data/Session.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using Caliburn.Micro;
 using NZazuFiddle.Annotations;
