17e8d61822d8dd9a5fb23ee826b93d2ec91d3fdd
Jan Bornemeier <jan.bornemeier@schoenhofer.de>
2018-03-08 10:30:04 +0100

    Add "Delete all" functionality.

--- NZazuFiddle/FileMenuViewModel.cs
+++ NZazuFiddle/FileMenuViewModel.cs
@@ -5,6 +5,7 @@ using NZazuFiddle.TemplateManagement;
 using NZazuFiddle.TemplateManagement.Contracts;
 using System;
 using System.Linq;
+using System.Windows;
 
 namespace NZazuFiddle
 {
@@ -14,16 +15,19 @@ namespace NZazuFiddle
         private readonly ISession _session;
         private readonly ITemplateFileIoDialogService _fileIo;
         private readonly IWindowManager _windowManager;
+        private readonly ITemplateDbClient _templateDbRepo;
 
-        public FileMenuViewModel(ITemplateFileIoDialogService fileIo, ISession session, IWindowManager windowManager)
+        public FileMenuViewModel(ITemplateFileIoDialogService fileIo, ISession session, IWindowManager windowManager, ITemplateDbClient templateDbRepo)
         {
             _fileIo = fileIo ?? throw new ArgumentNullException(nameof(fileIo));
             _session = session ?? throw new ArgumentNullException(nameof(session));
             _windowManager = windowManager ?? throw new ArgumentNullException(nameof(windowManager));
+            _templateDbRepo = templateDbRepo ?? throw new ArgumentNullException(nameof(templateDbRepo));
 
             ImportIcon = IconChar.LevelDown;
             ExportIcon = IconChar.HddO;
             NewFileIcon = IconChar.Plus;
+            DeleteAllIcon = IconChar.TrashO;
         }
 
         public IconChar ImportIcon { get; set; }
@@ -31,6 +35,8 @@ namespace NZazuFiddle
         public IconChar ExportIcon { get; set; }
 
         public IconChar NewFileIcon { get; set; }
+
+        public IconChar DeleteAllIcon { get; set; }
 
         public void ExportToFiles()
         {
@@ -61,6 +67,24 @@ namespace NZazuFiddle
             {
                 var newTemplateSample = new TemplateSample(newFileViewModel.SampleId, newFileViewModel.SampleId, new FormDefinition(), new FormData(), ETemplateStatus.New);
                 _session.AddSampleAsUniqueItem(newTemplateSample.Sample);
+            }
+        }
+
+        public void DeleteAll()
+        {
+            var r = MessageBox.Show(
+                "Do you really want to delete all templates from database?",
+                "Delete all",
+                MessageBoxButton.YesNo
+            );
+
+            if (r == MessageBoxResult.Yes)
+            {
+                foreach(ISample sample in _session.Samples)
+                {
+                    _templateDbRepo.DeleteData(sample);
+                }
+                _session.Samples.Clear();
             }
         }
 
