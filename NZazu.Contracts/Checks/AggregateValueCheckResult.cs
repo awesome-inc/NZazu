d6c7a0f47b039e755e86d866643988b1863091a3
ThomasLey <thomas-ley@outlook.de>
2019-01-14 14:07:15 +0100

    fixes "IsValid" aggregate value check result

--- NZazu.Contracts/Checks/AggregateValueCheckResult.cs
+++ NZazu.Contracts/Checks/AggregateValueCheckResult.cs
@@ -4,16 +4,28 @@ using System.Linq;
 
 namespace NZazu.Contracts.Checks
 {
+    /// <summary>
+    /// Implements an aggregation of <see cref="ValueCheckResult"/>.
+    /// 
+    /// The errors are merged to an <see cref="AggregateException"/>.
+    /// The messages are joins to one single message.
+    /// </summary>
     public class AggregateValueCheckResult : ValueCheckResult
     {
         public AggregateValueCheckResult(IEnumerable<ValueCheckResult> checkResults)
         {
             var checkResultsArray = checkResults.ToArray();
 
-            IsValid = checkResultsArray.Any(x => !x.IsValid);
+            // valid only when ALL results are valid
+            IsValid = checkResultsArray.All(x => x.IsValid);
 
-            var message = string.Join(", ", checkResultsArray.Select(x => x.Error.Message));
-            Error = new AggregateException(message, checkResultsArray.Select(x => x.Error));
+            var message = string.Join(", ", checkResultsArray
+                .Where(x => !x.IsValid)
+                .Select(x => x.Error.Message));
+
+            Error = new AggregateException(message, checkResultsArray
+                .Where(x => !x.IsValid)
+                .Select(x => x.Error));
         }
 
     }
